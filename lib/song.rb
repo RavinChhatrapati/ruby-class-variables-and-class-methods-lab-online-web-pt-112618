0adc4f52d793f9a6586f74293073dbb81b718e5d
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-19 21:00:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,7 +10,7 @@ def initialize(name, artist, genre)
   @artist = artist
   @genre = genre
 
-@@count += 1                   # good place to put in function for execution within class method, show below with self
+@@count += 1                   # good place to put in function for execution within class method,  which is shown below with self
 @@artists.push(artist)
 @@genres.push(genre)
 
